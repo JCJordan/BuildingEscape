6532b89543936893b95b4da77f98920f206bc1cb
JCJordanPC <jordan.craig2@hotmail.co.uk>
2018-08-14 19:46:16 +0100

    Created blueprints for pawn and gamemode. Created grabber component.

---
+++ Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,35 @@
+// Copyright JJCraig 2018
+
+#include "Grabber.h"
+#include "Gameframework/Actor.h"
+
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber for [%s] is active and ready"), *(GetOwner()->GetName()))
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
